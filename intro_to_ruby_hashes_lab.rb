fe8012447c4a075d143919e2177e65383488fada
Lizzie Chan <lchan217@gmail.com>
2019-01-29 02:26:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,12 +24,7 @@ end
 def monopoly_with_third_tier
 		monopoly = {
 	  :railroads => {pieces: 4,
-	     :names => {
-	       reading_railroad => {},
-	       pennsylvania_raiload => {},
-	       b_and_o_railroad => {}, 
-	       shortline => {}
-	     },
+	     :names => {	     },
 	     :rent_in_dollars => {
 	       one_piece_owned: 25,
 	       two_pieces_owned: 50, 
